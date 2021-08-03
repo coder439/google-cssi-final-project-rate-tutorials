ad96a9866b9e5fd8144c04e401b39f25f7c09a5b
Anant Kovil <anant.k1226@gmail.com>
2021-08-03 22:50:47 +0000

    fixing google button

---
+++ public/js/firebase.js
@@ -0,0 +1,16 @@
+console.log("js/firebase.js : Remember to replace the instructor's firebase keys with yours!")
+
+// TODO: Replace the following with your app's Firebase project configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyB3N_KhZ-yRfgaMRmArnfjNLUA3LQw7Vbs",
+    authDomain: "final-project-3575b.firebaseapp.com",
+    databaseURL: "https://final-project-3575b-default-rtdb.firebaseio.com",
+    projectId: "final-project-3575b",
+    storageBucket: "final-project-3575b.appspot.com",
+    messagingSenderId: "706469270408",
+    appId: "1:706469270408:web:022454abb0950aaa33a03d"
+  };
+// END TODO
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);

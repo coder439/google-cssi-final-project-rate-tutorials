daa83415738df21b9e50e2156ed3f3dff27d291e
coder439 <pavanalluri111@gmail.com>
2021-08-03 22:52:55 +0000

    add new course page working

---
+++ public/js/newCourse.js
@@ -0,0 +1,17 @@
+console.log("hello")
+
+function publishCourse(){
+    console.log("hihihi")
+    console.log("test")
+
+    const courseName = document.querySelector('#courseName')
+    const courseDescription = document.querySelector('#courseDescription')
+    console.log(courseName.value)
+    console.log(courseDescription.value)
+    firebase.database().ref(`courses/${courseName.value}`).push({
+        courseName: courseName.value,
+        courseDescription: courseDescription.value 
+    })
+
+
+}

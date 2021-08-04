0d44903669c971845ee2c623c657ea0de20fab1d
Anant Kovil <anant.k1226@gmail.com>
2021-08-04 23:38:05 +0000

    changed up newReview

--- public/js/newReview.js
+++ public/js/newReview.js
@@ -1,30 +1,22 @@
-
-console.log("new review")
-console.log(document.cookie)
-const addReview = (courseName, reviewTitle, reviewBody) => {
-   const courseRef = firebase.database().ref(`courses/${courseName}`)
-   courseRef.on('value',(snapshot) => {
-     value = snapshot.val()
-     console.log(value)
-     const classKey = Object.keys(value)[0]
-     firebase.database().ref(`courses/${courseName}/{classKey}/reviews`).push({
-         reviewTitle: reviewTitle,
-         reviewBody: reviewBody
-     })
-   })
-}
-function filterByLabel() {
-  const findLabel = document.querySelector("#findLabel");
-  const findLabelVal = "." + findLabel.value;
-  const notes = document.querySelectorAll(findLabelVal);
-
-  let cards = ``;
-  for (var i = 0; i < notes.length; i++) {
-    cards += '<div class="column is-one-quarter">';
-    cards += notes[i].outerHTML;
-    cards += "</div>";
-  }
-
-  document.querySelector("#app").innerHTML = cards;
-  findLabel.value = "";
+const reviewDescription = document.querySelector('#reviewDescription').innerHTML
+let params = (new URL(document.location)).searchParams;
+let courseNameValue = params.get("courseName");
+courseNameh2 = document.querySelector('#courseName')
+courseNameh2.innerHTML = courseNameValue
+ 
+const publishReview = () => {
+   const reviewDescription = document.querySelector('#reviewDescription').innerHTML
+   let params = (new URL(document.location)).searchParams;
+   let courseNameValue = params.get("courseName");
+   console.log(courseNameValue)
+  const courseRef = firebase.database().ref(`courses/${courseNameValue}`)
+  courseRef.on('value',(snapshot) => {
+    const value = snapshot.val()
+    console.log(value)
+    const classKey = Object.keys(value)[0]
+    firebase.database().ref(`courses/${coursecourseNameValueName}/${classKey}/reviews`).push({
+        courseName: courseNameValue,
+        reviewBody: reviewDescription
+    })
+  })
 }

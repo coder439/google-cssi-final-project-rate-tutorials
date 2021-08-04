4ccb0d0a209c9c6e3e304ea83119238fc9e812d5
coder439 <pavanalluri111@gmail.com>
2021-08-04 23:40:54 +0000

    test

--- public/js/coursePage.js
+++ public/js/coursePage.js
@@ -2,6 +2,10 @@ document.querySelector("#name").innerHTML = "DSA"
 document.querySelector("#courseDescription").innerHTML = "learning "
 console.log("hello")
 console.log("test")
-const courseName = document.querySelector("#name")
-document.cookie = `${courseName.innerHTML};path=/newReview.html`;
-console.log(document.cookie)
+const courseName = document.querySelector("#name").innerHTML
+console.log(courseName)
+
+function addNewReviewPage(){
+    window.location = `newReview.html?courseName=${courseName}`
+
+}
--- public/js/newReview.js
+++ public/js/newReview.js
@@ -1,15 +1,22 @@
+const reviewDescription = document.querySelector('#reviewDescription').innerHTML
+let params = (new URL(document.location)).searchParams;
+let courseNameValue = params.get("courseName");
+courseNameh2 = document.querySelector('#courseName')
+courseNameh2.innerHTML = courseNameValue
 
-console.log("new review")
-console.log(document.cookie)
-const addReview = (courseName, reviewTitle, reviewBody) => {
-   const courseRef = firebase.database().ref(`courses/${courseName}`)
+const publishReview = () => {
+    const reviewDescription = document.querySelector('#reviewDescription').innerHTML
+    let params = (new URL(document.location)).searchParams;
+    let courseNameValue = params.get("courseName");
+    console.log(courseNameValue)
+   const courseRef = firebase.database().ref(`courses/${courseNameValue}`)
    courseRef.on('value',(snapshot) => {
-     value = snapshot.val()
+     const value = snapshot.val()
      console.log(value)
      const classKey = Object.keys(value)[0]
-     firebase.database().ref(`courses/${courseName}/{classKey}/reviews`).push({
-         reviewTitle: reviewTitle,
-         reviewBody: reviewBody
+     firebase.database().ref(`courses/${coursecourseNameValueName}/${classKey}/reviews`).push({
+         courseName: courseNameValue,
+         reviewBody: reviewDescription
      })
    })
 }

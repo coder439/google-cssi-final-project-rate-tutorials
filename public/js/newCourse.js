f54e7772874ecd36a61963ad6f911d43fa954d5e
Anant Kovil <anant.k1226@gmail.com>
2021-08-04 00:11:49 +0000

    Added image to new course + made it look neater

--- public/js/newCourse.js
+++ public/js/newCourse.js
@@ -6,15 +6,19 @@ function publishCourse(){
 
     const courseName = document.querySelector('#courseName')
     const courseDescription = document.querySelector('#courseDescription')
+    const courseImage = document.querySelector("#courseImage")
     console.log(courseName.value)
     console.log(courseDescription.value)
+    console.log(courseImage.value)
     firebase.database().ref(`courses/${courseName.value}`).push({
         courseName: courseName.value,
         courseDescription: courseDescription.value 
+        courseImage: courseImage.value
     })
       .then(() => {
     courseName.value = "";
     courseDescription.value = "";
+    courseImage.value = "";
   });
 
 

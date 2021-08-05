800fb2fd2731f699c33e82b80098ebb5937608e2
coder439 <pavanalluri111@gmail.com>
2021-08-05 22:02:23 +0000

    continuing clean up

--- public/js/index.js
+++ public/js/index.js
@@ -20,8 +20,7 @@ var courses = getArrayOfCourseNames()
 
 
 console.log("index.js running")
-function dummyMethod(){
-    console.log("dummy method")
+function navigateToCourse(){
     const courseName = document.querySelector('#myInput');
     console.log(courseName.value)
     location.href = `coursePage.html?courseName=${courseName.value}`
